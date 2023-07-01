0e5e63d8a893d656366cca486315ee41e32cc084
Vlad Tarnovski <vladtarnovski989@gmail.com>
2023-07-01 14:51:02 +0300

    feat: add mock lib api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,55 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
+  const commentDataMock = {
+    postId: 1,
+    id: 2,
+    name: 'quo vero reiciendis velit similique earum',
+    email: 'Jayne_Kuhic@sydney.com',
+    body: 'est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et',
+  };
+
+  const baseUrl = 'https://jsonplaceholder.typicode.com';
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const mockAxios = jest.spyOn(axios, 'create');
+
+    mockAxios.mockReturnValue({
+      get: async () => ({ commentDataMock }),
+    } as never);
+
+    await throttledGetDataFromApi('/comments/2');
+    expect(mockAxios).toHaveBeenCalledWith({
+      baseURL: baseUrl,
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const mockedFn = jest.fn(async () => ({ commentDataMock }));
+
+    const axiosMock = jest.spyOn(axios, 'create');
+    axiosMock.mockReturnValue({
+      get: mockedFn,
+    } as never);
+
+    await throttledGetDataFromApi('/comments/2');
+    jest.runAllTimers();
+
+    expect(mockedFn).toHaveBeenLastCalledWith('/comments/2');
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    jest.runAllTimers();
+    const content = await throttledGetDataFromApi('/comments/2');
+    expect(JSON.stringify(commentDataMock)).toBe(JSON.stringify(content));
   });
 });
